f2acd79d2ae83ebb0fca9249637448d3be8f4223
JoseOC86 <joc.olmos@gmail.com>
2025-10-16 21:11:47 +0200

    feat: complete competition register

--- src/main/java/com/jolmos/ticarum/sport/competitions/infrastructure/controller/CompetitionController.java
+++ src/main/java/com/jolmos/ticarum/sport/competitions/infrastructure/controller/CompetitionController.java
@@ -1,20 +1,30 @@
 package com.jolmos.ticarum.sport.competitions.infrastructure.controller;
 
 import com.jolmos.ticarum.sport.competitions.aplication.usecase.RegistrarCompeticionUseCase;
-import com.jolmos.ticarum.sport.competitions.domain.model.Competicion;
 import com.jolmos.ticarum.sport.competitions.infrastructure.controller.dto.CompetitionDTO;
 import com.jolmos.ticarum.sport.competitions.infrastructure.controller.mapper.CompetitionDTOMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
+
 @RestController
+@RequestMapping ("/api/competition")
 @RequiredArgsConstructor
 public class CompetitionController {
 
     private final RegistrarCompeticionUseCase registrarCompeticionUseCase;
     private final CompetitionDTOMapper competitionDTOMapper;
-    public ResponseEntity<Competicion> registrar(CompetitionDTO competicion) {
-        return ResponseEntity.created("/competition/" + this.registrarCompeticionUseCase.execute(this.competitionDTOMapper.toCompeticion(competicion)));
+
+    @PostMapping
+    public ResponseEntity<CompetitionDTO> registrar(@Valid @RequestBody CompetitionDTO competicion) {
+        CompetitionDTO competitionDTO = competitionDTOMapper.toCompetitionDTO(this.registrarCompeticionUseCase.execute(this.competitionDTOMapper.toCompeticion(competicion)));
+        return ResponseEntity.created(URI.create("/api/competition/" + competitionDTO.id())).body(competitionDTO);
     }
 }
--- src/main/java/com/jolmos/ticarum/sport/competitions/infrastructure/controller/dto/CompetitionDTO.java
+++ src/main/java/com/jolmos/ticarum/sport/competitions/infrastructure/controller/dto/CompetitionDTO.java
@@ -1,6 +1,17 @@
 package com.jolmos.ticarum.sport.competitions.infrastructure.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record CompetitionDTO(Long id, String nombre, String deporte, LocalDate fechaInicio, LocalDate fechaFin, int pistasDisponibles) {
+public record CompetitionDTO(@JsonIgnore Long id,
+                             @NotBlank String nombre,
+                             @NotBlank String deporte,
+                             @NotNull LocalDate fechaInicio,
+                             @NotNull LocalDate fechaFin,
+                             @Min(value = 1) @Max(value = 100) int pistasDisponibles) {
 }
